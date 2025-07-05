3cb928d7d1b184ef01df99f7618bd8cc3b0c7652
deverick <dncellassistec@gmail.com>
2025-07-05 19:07:52 -0300

    criado pagina notfald

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 
-
 export function Notfound() {
   return (
-    <div>
-      <h1>Pafina não encontrada</h1>
-      <Link to={"/"}>
-      Ir para página Home
-      </Link>
-    </div>
+    <main className="w-full  ">
+      <div className="w-full max-w-6xl  flex items-center justify-center px-5 mx-auto mt-10 gap-1">
+        <h1 className="font-mediun text-lg">Pagina não encontrada</h1>
+        <Link className="text-lg hover:text-blue-600 text-purple-900" to={"/"}>Ir para página Home</Link>
+      </div>
+    </main>
   );
 }
