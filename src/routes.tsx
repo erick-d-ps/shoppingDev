708779e087f1d41fcc65b27806611b1af22e58b3
deverick <dncellassistec@gmail.com>
2025-07-20 19:20:33 -0300

    adicionando pagina /detail/:id

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Layout } from "./components/layout";
 import { Notfound } from "./pages/notfound";
+import { Detail } from "./pages/detail";
 
 const Router = createBrowserRouter([
   {
@@ -13,9 +14,14 @@ const Router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/detail/:id",
+        element:<Detail/>
+      },
+      {
         path: "*",
         element: <Notfound/>
-      }
+      },
+    
     ],
   },
 ]);
