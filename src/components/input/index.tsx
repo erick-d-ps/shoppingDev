ed7a210d835bc4821270c8d9cb477d384bfff8ab
deverick <dncellassistec@gmail.com>
2025-08-29 23:13:06 -0300

    start of input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,11 @@
+export function Input(){
+    return(
+       <div>
+         <input 
+           className="w-full border-gray-400 border-2 rounded-md py-0.5 px-2"
+           type="text" 
+           placeholder="digite a email"
+         />
+       </div> 
+    )
+}
