9830d4774b33df16c9f009650821dd241861cad0
deverick <dncellassistec@gmail.com>
2025-08-31 19:06:13 -0300

    end of the input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,11 +1,25 @@
-export function Input(){
+import {type RegisterOptions, type UseFormRegister } from "react-hook-form"
+
+interface ImputProps{
+  type: string;
+  placeholder: string;
+  name: string;
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions; 
+}
+
+export function Input({type, placeholder, name, register, error, rules}: ImputProps){
     return(
        <div>
          <input 
            className="w-full border-gray-400 border-2 rounded-md py-0.5 px-2"
-           type="text" 
-           placeholder="digite a email"
+           placeholder={placeholder}
+           type={type}
+           {...register(name, rules)}
+           id={name}
          />
+         {error && <p className="my-1 text-red-500">{error}</p>}
        </div> 
     )
 }
