ee56c5d9c633dcc33f9f1eb13916c9a6b8a4d0fe
deverick <dncellassistec@gmail.com>
2025-08-31 19:07:59 -0300

    end of interface and login page form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,28 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/input";
 
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.email("Incira um email válido").nonempty(),
+  password: z.string().nonempty("A senha é obrigatória!"),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
+
+  function onsubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <main className="w-full h-screen">
       <div className="flex flex-col w-full h-full items-center justify-center">
@@ -10,11 +31,36 @@ export function Login() {
             ShoppingDev
           </Link>
         </div>
-        <form className=" flex flex-col w-full items-center justify-center max-w-xl bg-white mt-5 p-2 rounded-md">
-          <div className="w-full">
-            <Input/>
+        <form
+          onSubmit={handleSubmit(onsubmit)}
+          className=" flex flex-col w-full items-center justify-center max-w-xl bg-white mt-5 p-2 rounded-md"
+        >
+          <div className="w-full m-5">
+            <Input
+              name="email"
+              placeholder="Digite o email..."
+              type="email"
+              error={errors.email?.message}
+              register={register}
+            />
           </div>
+          <div className="w-full">
+            <Input
+              name="password"
+              placeholder="Digite a senha..."
+              type="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-5 bg-fuchsia-700 text-white font-bold py-1 rounded-md"
+          >
+            Acessar
+          </button>
         </form>
+        <p className="mt-5">Ainda não tem um conta <Link className="text-fuchsia-700 font-medium" to={"/register"}>Cadastre-se</Link></p>
       </div>
     </main>
   );
