6ad147c781096d088290d66da0f8895f64d51a29
deverick <dncellassistec@gmail.com>
2025-09-01 17:15:20 -0300

    installed heat-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ShoppingProvider from './context/index.tsx'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ShoppingProvider from "./context/index.tsx";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { Toaster } from "react-hot-toast";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   <ShoppingProvider>
-    <App />
-   </ShoppingProvider>
-  </StrictMode>,
-)
+    <ShoppingProvider>
+      <Toaster position="top-right" reverseOrder={false} />
+      <App />
+    </ShoppingProvider>
+  </StrictMode>
+);
