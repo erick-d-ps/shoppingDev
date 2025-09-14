083fdbbe1dcad5aeca1b41410329da0a0af0c548
deverick <dncellassistec@gmail.com>
2025-09-14 18:14:30 -0300

    added .env

--- src/services/firebase/firebaseConection.ts
+++ src/services/firebase/firebaseConection.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDm7GBSDLZjA8x-2P-cahHMMK-i9vpcq0",
-  authDomain: "shopppingdev.firebaseapp.com",
-  projectId: "shopppingdev",
-  storageBucket: "shopppingdev.firebasestorage.app",
-  messagingSenderId: "408809069135",
-  appId: "1:408809069135:web:2e2a44b310c8803243683a"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 
